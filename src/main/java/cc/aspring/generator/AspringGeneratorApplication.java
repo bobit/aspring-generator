0c4a45c87a2b5d5884618a99ebe6de4a7851a1fa
bobit <iloveapi@163.com>
2019-08-28 14:50:59 +0800

    Initial commit

---
+++ src/main/java/cc/aspring/generator/AspringGeneratorApplication.java
@@ -0,0 +1,13 @@
+package cc.aspring.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspringGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AspringGeneratorApplication.class, args);
+    }
+
+}
